51d66d656e1960fb1cb2e1fea34e5876f3a5d681
Raymond Chen <ahrchen@gmail.com>
2022-02-10 11:47:40 -0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Raymond Chen on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
